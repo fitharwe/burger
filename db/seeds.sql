a5c25693b4b2440d104f42f7309ad7671e92c1be
fitharwe <jesterenwalker@gmail.com>
2018-06-21 21:11:31 -0400

    further structure

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Thai Chia Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spinach Artichoke Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black & Bleu Burger');
+
+INSERT INTO menu (item) VALUES ('Turkey Burger');
+INSERT INTO menu (item) VALUES ('Thai Chia Burger');
+INSERT INTO menu (item) VALUES ('Spinach Artichoke Turkey Burger');
+INSERT INTO menu (item) VALUES ('Grilled Teriyaki Chicken Burger');
+INSERT INTO menu (item) VALUES ('Portobello Bacon Burger');
+INSERT INTO menu (item) VALUES ('Cheeseburger');
+INSERT INTO menu (item) VALUES ('Cowboy Whiskey Burger');
+INSERT INTO menu (item) VALUES ('Garden & shroom Burger');
+INSERT INTO menu (item) VALUES ('Chef Special Burger');
+INSERT INTO menu (item) VALUES ('Monster Burger');
+
+INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Cheese Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Garden Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Caesar Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Curly Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Waffle Fries', 0);
